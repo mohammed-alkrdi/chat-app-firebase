db339ced5dfe58df277529f5fa78dd68bf70d466
mohammed alkrdi <makkalkrdi7770@gmail.com>
2022-10-04 10:13:35 +0300

    commit

--- lib/widgets/loader.dart
+++ lib/widgets/loader.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:full_chat_firebase/constances/colors.dart';
 
 class Loader extends StatelessWidget {
